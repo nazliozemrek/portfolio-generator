c2158076d5c650ae79a49ba0bafd13b11864400d
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-04-19 10:59:42 -0500

    gitignore file

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
 
-const [name1,github] = profileDataArgs;
+const inquirer = require('inquirer');
 
-const fs= require('fs');
+inquirer 
+    .prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message:'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
+// const pageHTML = generatePage(name1, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-fs.writeFile('index.html',generatePage(name1,github),err => {
-    if(err) throw new err;
-    console.log('Portfolio Completed! Checkout index.html to see output!') 
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
